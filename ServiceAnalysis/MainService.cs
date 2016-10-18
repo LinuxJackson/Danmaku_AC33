1bd0d89950478f3850075cc408ac88b9a275f013
LinuxJackson <2227941752@qq.com>
2016-10-18 10:50:29 +0800

    First Push

---
+++ ServiceAnalysis/MainService.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using libNetwork.Sockets;
+using System.Net.Sockets;
+using libInvoker;
+using System.Threading;
+using Model;
+
+namespace ServiceAnalysis
+{
+    public class MainService
+    {
+        public void Start()
+        {
+            while (true)
+            {
+                TcpClient client = GetClient.Get();
+
+                new Thread(delegate ()
+                {
+                    try
+                    {
+                        SockReceiver rece = new SockReceiver();
+                        MainBLL main = new MainBLL();
+                        while (true)
+                            main.Analysis(rece.ReceiveData(client));
+                    }
+                    catch
+                    {
+                        Thread.CurrentThread.Abort();
+                    }
+                }).Start();
+            }
+        }
+    }
+}
---
+++ libInvoker/Invokers/CreateRoom.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Model;
+using Model.Structs;
+using libNetwork.Sockets;
+using MemoryData;
+
+namespace libInvoker.Invokers
+{
+    internal class CreateRoom
+    {
+        Room_mod room = new Room_mod();
+        System.Net.Sockets.TcpClient client;
+        MainBLL BASE;
+
+        internal CreateRoom(DataPackage data, MainBLL BASE)
+        {
+            room.FromBytes(data.Data);
+            client = data.Client;
+            this.BASE = BASE;
+        }
+
+        internal void Response()
+        {
+            try
+            {
+                SockSender sender = new SockSender();
+                if (room.User_id == "" || room.Room_name == "")
+                {
+                    sender.SendMessage(new DataPackage
+                    {
+                        Client = client,
+                        Data = room.ToBytes(libData.ConstData.OP_code_CreateRoom_Failed, room.Uid)
+                    });
+                    return;
+                }
+                if (RoomControl.Rooms.ContainsKey(room.Room_name))
+                {
+                    sender.SendMessage(new DataPackage
+                    {
+                        Client = client,
+                        Data = room.ToBytes(libData.ConstData.OP_code_CreateRoom_NameTaken,room.Uid)
+                    });
+                    return;
+                }
+
+                BASE.user.Room_id = room.Room_name;
+                BASE.user.User_id = room.User_id;
+                BASE.user.Client = client;
+
+                lock (RoomControl.Rooms)
+                {
+                    RoomControl.Rooms.Add(room.Room_name, new Room
+                    {
+                        EoomInfo = room,
+                    });
+                    Room r = RoomControl.Rooms[room.Room_name];
+                    r.Users.Add(BASE.user.User_id, BASE.user);
+                }
+                ExitRoom exit = new ExitRoom();
+                exit.Exit(BASE);
+
+                sender.SendMessage(new DataPackage
+                {
+                    Client = client,
+                    Data = room.ToBytes(libData.ConstData.OP_code_CreateRoom_Succeed, room.Uid)
+                });
+                return;
+            }
+            catch
+            {
+                SockSender sender = new SockSender();
+                sender.SendMessage(new DataPackage
+                {
+                    Client = client,
+                    Data = room.ToBytes(libData.ConstData.OP_code_CreateRoom_Failed, room.Uid)
+                });
+            }
+        }
+    }
+}
